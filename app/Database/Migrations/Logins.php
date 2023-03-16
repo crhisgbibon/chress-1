a66c6b8a96ef7e8b4f37bb27480f97935abe95a7
crhisgbibon <admin@crhisgbibon.com>
2023-03-16 00:30:23 +0000

    16/03/23 fix migration

--- app/Database/Migrations/Logins.php
+++ app/Database/Migrations/Logins.php
@@ -17,39 +17,23 @@ class Logins
 
   public function up()
   {
-    try
-    {
-      $result = $this->db->pdo->query("SELECT 1 FROM `logins` LIMIT 1");
+    try {
+      $stmt = $this->db->pdo->prepare("CREATE TABLE IF NOT EXISTS `logins`(
+        `uniqueIndex` INT (11) PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+        `user_alias` VARCHAR (50) NOT NULL,
+        `user_password` VARCHAR(255) NOT NULL,
+        `createdAt` DATETIME,
+        `user_email` VARCHAR(255) NOT NULL,
+        `user_status` VARCHAR(100) NOT NULL,
+        `activationCode` VARCHAR(255) NOT NULL,
+        `activationExpiry` DATETIME,
+        `isVerified` TINYINT(4) NOT NULL)");
+      $stmt->execute();
+      printf('Logins table created'."\n");
     }
-    catch(Exception $e)
+    catch(PDOException $e)
     {
-      $result = false;
-    }
-
-    if($result === false)
-    {
-      try {
-        $stmt = $this->db->pdo->prepare("CREATE TABLE `logins`(
-          `uniqueIndex` INT (11) PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
-          `user_alias` VARCHAR (50) NOT NULL,
-          `user_password` VARCHAR(255) NOT NULL,
-          `createdAt` DATETIME,
-          `user_email` VARCHAR(255) NOT NULL,
-          `user_status` VARCHAR(100) NOT NULL,
-          `activationCode` VARCHAR(255) NOT NULL,
-          `activationExpiry` DATETIME,
-          `isVerified` TINYINT(4) NOT NULL)");
-        $stmt->execute();
-        printf('Logins table created'."\n");
-      }
-      catch(PDOException $e)
-      {
-        printf('Unable to create Logins table'."\n");
-      }
-    }
-    else
-    {
-      printf('Logins table already exists'."\n");
+      printf('Unable to create Logins table'."\n");
     }
   }
 }
--- app/Database/Migrations/Tokens.php
+++ app/Database/Migrations/Tokens.php
@@ -17,37 +17,21 @@ class Tokens
 
   public function up()
   {
-    try
-    {
-      $result = $this->db->pdo->query("SELECT 1 FROM `logins` LIMIT 1");
+    try {
+      $stmt = $this->db->pdo->prepare("CREATE TABLE IF NOT EXISTS `tokens`(
+        `uniqueIndex` INT (11) PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+        `user_alias` VARCHAR (255) NOT NULL,
+        `deviceID` VARCHAR(255) NOT NULL,
+        `passwordHash` VARCHAR(255) NOT NULL,
+        `selectorHash` VARCHAR(255) NOT NULL,
+        `isExpired` INT (11) NOT NULL,
+        `expiryDate` VARCHAR(45) NOT NULL)");
+      $stmt->execute();
+      printf('Tokens table created'."\n");
     }
-    catch(Exception $e)
+    catch(PDOException $e)
     {
-      $result = false;
-    }
-
-    if($result === false)
-    {
-      try {
-        $stmt = $this->db->pdo->prepare("CREATE TABLE `tokens`(
-          `uniqueIndex` INT (11) PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
-          `user_alias` VARCHAR (255) NOT NULL,
-          `deviceID` VARCHAR(255) NOT NULL,
-          `passwordHash` VARCHAR(255) NOT NULL,
-          `selectorHash` VARCHAR(255) NOT NULL,
-          `isExpired` INT (11) NOT NULL,
-          `expiryDate` VARCHAR(45) NOT NULL)");
-        $stmt->execute();
-        printf('Tokens table created'."\n");
-      }
-      catch(PDOException $e)
-      {
-        printf('Unable to create Tokens table'."\n");
-      }
-    }
-    else
-    {
-      printf('Tokens table already exists'."\n");
+      printf('Unable to create Tokens table'."\n");
     }
   }
 }
